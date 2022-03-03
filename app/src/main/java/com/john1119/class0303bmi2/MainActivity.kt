99d9f31265c1ba728676cc42a00725d2cbe7cf43
John1119_Chen <john1119_chen@mail.chungyo.net>
2022-03-03 16:47:57 +0800

    Class0303 latest

--- app/src/main/java/com/john1119/class0303bmi2/MainActivity.kt
+++ app/src/main/java/com/john1119/class0303bmi2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.john1119.class0303bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.john1119.class0303bmi2.databinding.ActivityMainBinding //幫忙import出來的
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+//    lateinit等一下畫面生出來才給
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun bmi(view:View){//View不要自己打完因為他會幫忙import
+        println("hahaha")
+        var weight=binding.edWeight.text.toString().toFloat()
+        var height=binding.edHeight.text.toString().toFloat()
     }
 }
